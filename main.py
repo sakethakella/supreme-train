27d9ff30ce8b225674042b591f18888454867d9c
Akella Satya Hanuma Saketh <63924046+sakethakella@users.noreply.github.com>
2026-03-14 16:25:37 +0530

    minor mispaces solved

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import csp
 
 #display a graph and animations of the solver and other things also a website can be created for this purpose where one can input a array for this visaully
